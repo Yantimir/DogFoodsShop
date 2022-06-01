bfe8986bfe46976215963785235589e4899b1918
Yantimir <mingazitdinov@mail.ru>
2022-06-01 22:31:26 +0400

    build

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,14 @@ const HTMLWebpackPlugins = require("html-webpack-plugin");
 module.exports = {
     entry: path.resolve(__dirname, "src/index.js"),//точка входа в наше приложение содержит абсолютный путь к index.js
     output: {
-        path: path.resolve(__dirname, "dist"),//путь куда будет собираться наш проект
+        path: path.resolve(__dirname, "build"),//путь куда будет собираться наш проект
         filename: "main.js",// имя нашего бандла
         publicPath: "/"
     },
     mode: "development",// по умолчанию webpack миницифирует скрипты, чтобы это избежать меням режим
     devServer: {
         historyApiFallback: true,
-        static: path.resolve(__dirname, "./dist"), // путь, куда "смотрит" режим разработчика
+        static: path.resolve(__dirname, "./build"), // путь, куда "смотрит" режим разработчика
         compress: true, // это ускорит загрузку в режиме разработки
         port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
         open: true, // сайт будет открываться сам при запуске npm run dev
